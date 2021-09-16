172f0aed9f147178c7fb8f42a40271d09e8489b6
deltagare <iogunjobi@gmail.com>
2021-09-17 00:58:40 +0200

    Initial commit

---
+++ src/main/java/se/lecicon/jpaassignment_recipeedatabase/JpaAssignmentRecipeeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package se.lecicon.jpaassignment_recipeedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssignmentRecipeeDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAssignmentRecipeeDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lecicon/jpaassignment_recipeedatabase/JpaAssignmentRecipeeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lecicon.jpaassignment_recipeedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAssignmentRecipeeDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
